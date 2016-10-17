884a5d944631c959ea661332f52a48943ca34cbf
Martijn van Dijk <mhvdijk@gmail.com>
2016-10-17 14:06:02 +0200

    Update sample and binder to access native implementation

--- Sample/MediaSample.Droid/MainActivity.cs
+++ Sample/MediaSample.Droid/MainActivity.cs
@@ -7,6 +7,9 @@ using Android.Support.V7.Widget;
 using Android.Widget;
 using System;
 using Android.Graphics;
+using Plugin.MediaManager;
+using Android.Media;
+using Android.Support.V4.Media.Session;
 
 namespace MediaSample.Droid
 {
@@ -60,29 +63,49 @@ namespace MediaSample.Droid
             var seekbar = FindViewById<SeekBar>(Resource.Id.player_seekbar);
             ViewModel.MediaPlayer.Playing += (object sender, EventArgs e) =>
             {
-                seekbar.Max = ViewModel.Duration;
-                seekbar.Progress = ViewModel.Position;
+                RunOnUiThread(() =>
+                {
+                    seekbar.Max = ViewModel.Duration;
+                    seekbar.Progress = ViewModel.Position;
 
-                position.Text = ViewModel.GetFormattedTime(ViewModel.Position);
-                duration.Text = ViewModel.GetFormattedTime(ViewModel.Duration);
+                    position.Text = ViewModel.GetFormattedTime(ViewModel.Position);
+                    duration.Text = ViewModel.GetFormattedTime(ViewModel.Duration);
+                });
             };
 
             ViewModel.MediaPlayer.Buffering += (object sender, EventArgs e) =>
             {
-                seekbar.SecondaryProgress = ViewModel.MediaPlayer.Buffered;
+                RunOnUiThread(() =>
+                {
+                    seekbar.SecondaryProgress = ViewModel.MediaPlayer.Buffered;
+                });
             };
 
             ViewModel.MediaPlayer.CoverReloaded += (object sender, EventArgs e) =>
             {
-                var cover = FindViewById<ImageView>(Resource.Id.imageview_cover);
-                cover.SetImageBitmap(ViewModel.Cover as Bitmap);
+                RunOnUiThread(() =>
+                {
+                    var cover = FindViewById<ImageView>(Resource.Id.imageview_cover);
+                    cover.SetImageBitmap(ViewModel.Cover as Bitmap);
+                });
             };
 
             var title = FindViewById<TextView>(Resource.Id.textview_title);
             var subtitle = FindViewById<TextView>(Resource.Id.textview_subtitle);
             ViewModel.MediaPlayer.StatusChanged += (object sender, EventArgs e) =>
             {
-                
+                var mediaPlayer = ((MediaManagerImplementation)ViewModel.MediaPlayer).Binder.GetMediaPlayerService();
+
+                var metadata = mediaPlayer.mediaControllerCompat.Metadata;
+                if (metadata != null)
+                {
+                    RunOnUiThread(() =>
+                    {
+                        title.Text = metadata.GetString(MediaMetadata.MetadataKeyTitle);
+                        subtitle.Text = metadata.GetString(MediaMetadata.MetadataKeyArtist);
+                        playpause.Selected = mediaPlayer.mediaControllerCompat.PlaybackState.State == PlaybackStateCompat.StatePlaying;
+                    });
+                }
             };
 
             ViewModel.MediaPlayer.Play("http://www.montemagno.com/sample.mp3");
