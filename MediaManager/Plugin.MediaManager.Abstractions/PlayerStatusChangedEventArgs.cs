59def02ca7f161bad3c1db48944de7dc30757112
Erlend <erlend.angelsen@gmail.com>
2016-10-22 22:14:59 +0200

    Create custom eventargs for events

---
+++ MediaManager/Plugin.MediaManager.Abstractions/PlayerStatusChangedEventArgs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Plugin.MediaManager.Abstractions
+{
+    public class PlayerStatusChangedEventArgs : EventArgs
+    {
+        public PlayerStatusChangedEventArgs(PlayerStatus status)
+        {
+            Status = status;
+        }
+        public PlayerStatus Status { get; }
+    }
+}
