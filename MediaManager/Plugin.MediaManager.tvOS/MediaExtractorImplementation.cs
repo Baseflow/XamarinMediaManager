25ea5b6146819510e03e16a8dcb296634ebce0e0
Erlend <erlend.angelsen@gmail.com>
2016-10-28 09:22:39 +0200

    Add missing features to macOS project

---
+++ MediaManager/Plugin.MediaManager.tvOS/MediaExtractorImplementation.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AVFoundation;
+using Foundation;
+using Plugin.MediaManager.Abstractions;
+using UIKit;
+
+namespace Plugin.MediaManager
+{
+    public class MediaExtractorImplementation : IMediaExtractor
+    {
+        public async Task<IMediaFile> ExtractMediaInfo(IMediaFile mediaFile)
+        {
+            try
+            {
+                var assetsToLoad = new List<string>
+                {
+                    AVMetadata.CommonKeyArtist,
+                    AVMetadata.CommonKeyTitle,
+                    AVMetadata.CommonKeyArtwork
+                };
+                var nsUrl = new NSUrl(mediaFile.Url);
+            
+                // Default title to filename
+                mediaFile.Title = nsUrl.LastPathComponent;
+            
+                var asset = AVAsset.FromUrl(nsUrl);
+                await asset.LoadValuesTaskAsync(assetsToLoad.ToArray());
+
+                foreach (var avMetadataItem in asset.CommonMetadata)
+                {
+                    if (avMetadataItem.CommonKey == AVMetadata.CommonKeyArtist)
+                    {
+                        mediaFile.Artist = ((NSString) avMetadataItem.Value).ToString();
+                    }
+                    else if (avMetadataItem.CommonKey == AVMetadata.CommonKeyTitle)
+                    {
+                        mediaFile.Title = ((NSString) avMetadataItem.Value).ToString();
+                    }
+                    else if (avMetadataItem.CommonKey == AVMetadata.CommonKeyArtwork)
+                    {
+                        var image = UIImage.LoadFromData(avMetadataItem.DataValue);
+                        mediaFile.Cover = image;
+                    }
+                }
+                return mediaFile;
+            }
+            catch (Exception)
+            {
+                return mediaFile;
+            }
+        }
+    }
+}
