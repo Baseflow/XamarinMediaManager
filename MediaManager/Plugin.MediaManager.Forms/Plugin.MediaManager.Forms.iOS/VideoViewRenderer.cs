f508621a7dec124602f0d874a987d01f591011f1
Erlend Angelsen <erlend.angelsen@evry.com>
2016-11-04 08:37:41 +0100

    Fixes for iOS forms sample

--- MediaManager/Plugin.MediaManager.Forms/Plugin.MediaManager.Forms.iOS/VideoViewRenderer.cs
+++ MediaManager/Plugin.MediaManager.Forms/Plugin.MediaManager.Forms.iOS/VideoViewRenderer.cs
@@ -1,4 +1,8 @@
-﻿using Plugin.MediaManager.Forms;
+﻿using System;
+using CoreGraphics;
+using Foundation;
+using Plugin.MediaManager.Abstractions.Implementations;
+using Plugin.MediaManager.Forms;
 using Plugin.MediaManager.Forms.iOS;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.iOS;
@@ -6,9 +10,18 @@ using Xamarin.Forms.Platform.iOS;
 [assembly: ExportRenderer(typeof(VideoView), typeof(VideoViewRenderer))]
 namespace Plugin.MediaManager.Forms.iOS
 {
+    [Preserve(AllMembers = true)]
     public class VideoViewRenderer : ViewRenderer<VideoView, VideoSurface> 
     {
         private VideoSurface _videoSurface;
+
+        /// <summary>
+        /// Used for registration with dependency service
+        /// </summary>
+        public async static void Init()
+        {
+            var temp = DateTime.Now;
+        }
 
         protected override void OnElementChanged(ElementChangedEventArgs<VideoView> e)
         {
@@ -16,9 +29,16 @@ namespace Plugin.MediaManager.Forms.iOS
             if (Control == null)
             {
                 _videoSurface = new VideoSurface();
+                CrossMediaManager.Current.VideoPlayer.SetAspectMode(VideoAspectMode.AspectFill);
                 SetNativeControl(_videoSurface);
                 CrossMediaManager.Current.VideoPlayer.SetVideoSurface(_videoSurface);
             }
+        }
+
+        public override void Draw(CGRect rect)
+        {
+            _videoSurface.Frame = rect;
+            base.Draw(rect);
         }
     }
 }
