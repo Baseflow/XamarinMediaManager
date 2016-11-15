969e4c1b7a636d28bdf16e96262efc843ddb7252
Martijn van Dijk <mhvdijk@gmail.com>
2016-11-15 14:36:23 +0100

    Add layout for mini player with animation

--- Samples/MvvmCross/MyMediaPlayer.Droid/Fragments/HomeFragment.cs
+++ Samples/MvvmCross/MyMediaPlayer.Droid/Fragments/HomeFragment.cs
@@ -23,56 +23,5 @@ namespace MyMediaPlayer.Droid.Fragments
     public class HomeFragment : BaseFragment<HomeViewModel>
     {
         protected override int FragmentId => Resource.Layout.fragment_home;
-        private BottomSheetBehavior mBottomSheetBehavior;
-
-        public override void OnViewCreated(View view, Bundle savedInstanceState)
-        {
-            base.OnViewCreated(view, savedInstanceState);
-
-            var parentThatHasBottomSheetBehavior = view.FindViewById<FrameLayout>(Resource.Id.bottomsheetview);
-            mBottomSheetBehavior = BottomSheetBehavior.From(parentThatHasBottomSheetBehavior);
-            if (mBottomSheetBehavior != null)
-            {
-                mBottomSheetBehavior.SetBottomSheetCallback(new PlayerBehavior(mBottomSheetBehavior, view));
-            }
-        }
-
-        private class PlayerBehavior : BottomSheetBehavior.BottomSheetCallback
-        {
-            BottomSheetBehavior bottomSheetBehavior;
-            View screenView;
-
-            public PlayerBehavior(BottomSheetBehavior bottom, View view)
-            {
-                bottomSheetBehavior = bottom;
-                screenView = view;
-            }
-
-            public override void OnSlide(View bottomSheet, float newState)
-            {
-                
-            }
-
-            public override void OnStateChanged(View bottomSheet, int slideOffset)
-            {
-                var toolbar = screenView.FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
-
-                switch ((bottomSheetBehavior.State))
-                {
-                    case BottomSheetBehavior.StateCollapsed:
-                        toolbar.Visibility = ViewStates.Visible;
-                        break;
-                    case BottomSheetBehavior.StateDragging:
-                        break;
-                    case BottomSheetBehavior.StateExpanded:
-                        break;
-                    case BottomSheetBehavior.StateHidden:
-                        break;
-                    case BottomSheetBehavior.StateSettling:
-                        toolbar.Visibility = ViewStates.Gone;
-                        break;
-                }
-            }
-        }
     }
 }
