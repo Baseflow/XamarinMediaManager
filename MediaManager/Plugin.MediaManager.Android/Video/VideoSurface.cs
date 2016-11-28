23eca1a4c0f82531d9f62ffefe9e24b455fad7d7
Martijn van Dijk <mhvdijk@gmail.com>
2016-11-28 21:53:32 +0100

    Update packages

--- MediaManager/Plugin.MediaManager.Android/Video/VideoSurface.cs
+++ MediaManager/Plugin.MediaManager.Android/Video/VideoSurface.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using Android.Content;
+using Android.Runtime;
+using Android.Util;
 using Android.Widget;
 using Plugin.MediaManager.Abstractions;
 
@@ -9,7 +11,22 @@ namespace Plugin.MediaManager
     {
         public VideoSurface(Context context) : base(context)
         {
-
         }
+
+		public VideoSurface(Context context, IAttributeSet attrs) : base(context, attrs)
+		{
+		}
+
+		public VideoSurface(Context context, IAttributeSet attrs, int defStyleAttr) : base(context, attrs, defStyleAttr)
+		{
+		}
+
+		public VideoSurface(Context context, IAttributeSet attrs, int defStyleAttr, int defStyleRes) : base(context, attrs, defStyleAttr, defStyleRes)
+		{
+		}
+
+		protected VideoSurface (IntPtr javaReference, JniHandleOwnership transfer) : base(javaReference, transfer)
+		{
+		}
     }
 }
