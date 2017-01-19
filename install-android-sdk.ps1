7b277ae6f14428886e34baf927923a401b3f8a79
Martijn van Dijk <mhvdijk@gmail.com>
2017-01-19 20:07:14 +0100

    Try android 24

--- install-android-sdk.ps1
+++ install-android-sdk.ps1
@@ -1,6 +1,10 @@
 $AndroidToolPath = "${env:ProgramFiles(x86)}\Android\android-sdk\tools\android" 
 #$AndroidToolPath = "$env:localappdata\Android\android-sdk\tools\android"
 
+environment:
+  ANDROID_HOME: "${env:ProgramFiles(x86)}\Android\android-sdk" 
+  JAVA_HOME: "C:\\Program Files\\Java\\jdk1.8.0"
+
 Function Get-AndroidSDKs() { 
     $output = & $AndroidToolPath list sdk --all 
     $sdks = $output |% { 
@@ -27,5 +31,11 @@ Function Install-AndroidSDK() {
     Echo 'y' | & $AndroidToolPath update sdk -u -a -t $sdkIndexArgument 
 }
 
-$sdks = Get-AndroidSDKs |? { $_.name -like 'sdk platform*API 24*' -or $_.name -like 'sdk platform*API 25*' } 
+install:
+  - echo y | "%ANDROID_HOME%\tools\android.bat" update sdk -u -a -t tools
+  - echo y | "%ANDROID_HOME%\tools\android.bat" update sdk -u -a -t platform-tools
+  - echo y | "%ANDROID_HOME%\tools\android.bat" update sdk -u -a -t build-tools-25.0.1
+  - echo y | "%ANDROID_HOME%\tools\android.bat" update sdk -u -a -t android-24
+
+$sdks = Get-AndroidSDKs |? { $_.name -like 'sdk platform*API 24*' -or $_.name -like 'google apis*api 24' } 
 Install-AndroidSDK -sdks $sdks
