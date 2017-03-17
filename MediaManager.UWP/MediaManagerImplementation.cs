a5f13316bab269296a16c3bed85f7412fc045c4b
Martijn van Dijk <mhvdijk@gmail.com>
2017-03-17 10:38:42 +0100

    Move files

---
+++ MediaManager.UWP/MediaManagerImplementation.cs
@@ -0,0 +1,30 @@
+using Plugin.MediaManager.Abstractions;
+using Plugin.MediaManager.Abstractions.Implementations;
+
+namespace Plugin.MediaManager
+{
+    /// <summary>
+    ///     Implementation for Feature
+    /// </summary>
+    public class MediaManagerImplementation : MediaManagerBase
+    {
+        private IAudioPlayer _audioPlayer;
+        private IVideoPlayer _videoPlayer;
+
+        public override IAudioPlayer AudioPlayer
+        {
+            get { return _audioPlayer ?? (_audioPlayer = new AudioPlayerImplementation(VolumeManager)); }
+            set { _audioPlayer = value; }
+        }
+
+        public override IVideoPlayer VideoPlayer
+        {
+            get { return _videoPlayer ?? (_videoPlayer = new VideoPlayerImplementation(VolumeManager)); }
+            set { _videoPlayer = value; }
+        }
+        public override IMediaNotificationManager MediaNotificationManager { get; set; } = new MediaNotificationManagerImplementation();
+        public override IMediaExtractor MediaExtractor { get; set; } = new MediaExtractorImplementation();
+
+        public override IVolumeManager VolumeManager { get; set; } = new VolumeManagerImplementation();
+    }
+}
