97340f6017203abcc8736116850ba94acf173bfd
Jonas Hodel <jonas.hodel@gmail.com>
2017-07-28 20:05:41 +0200

    MediaManager.UWP now provides support for Play/Pause, Next and Previous media buttons. This was tested on Win 10 using bluetooth speakers with physical media buttons. - Added RemoteControlNotificationHandler, including unit tests and system wrappers (that enable unit testing). - Integrated RemoteControlNotificationHandler with the MediaManagerImpementation for UWP.
    Removed pause, replaced with play/pause

---
+++ MediaManager.UWP/RemoteControlNotificationHandler.cs
@@ -0,0 +1,50 @@
+﻿using Windows.Media;
+using Plugin.MediaManager.Abstractions;
+using Plugin.MediaManager.SystemWrappers;
+
+namespace Plugin.MediaManager
+{
+    public class RemoteControlNotificationHandler
+    {
+        private readonly ISystemMediaTransportControlsWrapper _systemMediaTransportControlsWrapper;
+        private readonly IPlaybackController _playbackController;
+
+        public RemoteControlNotificationHandler(ISystemMediaTransportControlsWrapper systemMediaTransportControlsWrapper, IPlaybackController playbackController)
+        {
+            _systemMediaTransportControlsWrapper = systemMediaTransportControlsWrapper;
+            _playbackController = playbackController;
+
+            _systemMediaTransportControlsWrapper.IsNextEnabled = true;
+            _systemMediaTransportControlsWrapper.IsPreviousEnabled = true;
+            _systemMediaTransportControlsWrapper.IsPlayEnabled = true;
+        }
+
+        public void SubscribeToNotifications()
+        {
+            _systemMediaTransportControlsWrapper.SubscribeToMediaButtonEvents();
+            _systemMediaTransportControlsWrapper.ButtonPressed += MediaControls_ButtonPressed;
+        }
+
+        public void UnsubscribeFromNotifications()
+        {
+            _systemMediaTransportControlsWrapper.UnsubscribeFromMediaButtonEvents();
+            _systemMediaTransportControlsWrapper.ButtonPressed -= MediaControls_ButtonPressed;
+        }
+
+        private async void MediaControls_ButtonPressed(SystemMediaTransportControls sender, ISystemMediaTransportControlsButtonPressedEventArgsWrapper args)
+        {
+            switch (args.Button)
+            {
+                case SystemMediaTransportControlsButton.Next:
+                    await _playbackController.PlayNext();
+                    break;
+                case SystemMediaTransportControlsButton.Previous:
+                    await _playbackController.PlayPrevious();
+                    break;
+                case SystemMediaTransportControlsButton.Play:
+                    await _playbackController.PlayPause();
+                    break;
+            }
+        }
+    }
+}
---
+++ MediaManager.UWP/SystemWrappers/ISystemMediaTransportControlsWrapper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Windows.Foundation;
+using Windows.Media;
+
+namespace Plugin.MediaManager.SystemWrappers
+{
+    public interface ISystemMediaTransportControlsWrapper
+    {
+        void SubscribeToMediaButtonEvents();
+
+        void UnsubscribeFromMediaButtonEvents();
+
+        bool IsNextEnabled { get; set; }
+
+        bool IsPreviousEnabled { get; set; }
+
+        bool IsPauseEnabled { get; set; }
+
+        bool IsPlayEnabled { get; set; }
+
+        event TypedEventHandler<SystemMediaTransportControls, ISystemMediaTransportControlsButtonPressedEventArgsWrapper> ButtonPressed;
+    }
+}
