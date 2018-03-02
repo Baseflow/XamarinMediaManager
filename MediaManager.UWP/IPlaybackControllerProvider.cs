8c3dc9fbc3b77687bdca9e384e91828f3bcc4b7a
Casper Skoubo <casper@pockets.dk>
2018-03-02 12:57:57 +0100

    Clean up
    Fixed refs

--- MediaManager.UWP/IPlaybackControllerProvider.cs
+++
@@ -1,9 +0,0 @@
-﻿using Plugin.MediaManager.Abstractions;
-
-namespace Plugin.MediaManager
-{
-    internal interface IPlaybackControllerProvider
-    {
-        IPlaybackController PlaybackController { get; }
-    }
-}
--- MediaManager.UWP/ImageAsset.cs
+++
@@ -1,9 +0,0 @@
-﻿using Windows.UI.Xaml.Media.Imaging;
-using Plugin.MediaManager.Abstractions;
-
-namespace Plugin.MediaManager
-{
-    public class ImageAsset : object, IImageAsset
-    {
-    }
-}
