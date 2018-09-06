1175e2d4f5d1472a7daeadb2f82770525a84358f
Martijn van Dijk <mhvdijk@gmail.com>
2018-09-06 15:44:40 +0200

    Add some forms video views

---
+++ MediaManager.Forms/VideoPage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace MediaManager.Forms
+{
+    public class VideoPage : ContentPage
+    {
+        public VideoPage()
+        {
+            this.Content = new VideoView();
+        }
+    }
+}
