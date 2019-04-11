afaa77fdf89569740364face4720c786c6cd6eba
Martijn van Dijk <mhvdijk@gmail.com>
2019-04-11 20:35:04 +0200

    Fix id and names

---
+++ MediaManager.Forms/Platforms/Ios/VideoViewRenderer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Foundation;
+using MediaManager;
+using MediaManager.Forms;
+using MediaManager.Forms.Platforms.iOS;
+using MediaManager.Platforms.Ios.Video;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(VideoView), typeof(VideoViewRenderer))]
+namespace MediaManager.Forms.Platforms.iOS
+{
+    [Preserve(AllMembers = true)]
+    public class VideoViewRenderer : ViewRenderer<VideoView, MediaManager.Platforms.Ios.Video.VideoSurface> 
+    {
+        private MediaManager.Platforms.Ios.Video.VideoSurface _videoSurface;
+
+        public static void Init()
+        {
+            var temp = DateTime.Now;
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<VideoView> e)
+        {
+            base.OnElementChanged(e);
+            if (Control == null)
+            {
+                _videoSurface = new VideoSurface(Control);
+                SetNativeControl(_videoSurface);
+                CrossMediaManager.Current.MediaPlayer.SetPlayerView(_videoSurface);
+            }
+        }
+    }
+}
