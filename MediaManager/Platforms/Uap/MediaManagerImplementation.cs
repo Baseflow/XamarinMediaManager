78cecc260caaca11a662ac4df59786895b31e117
Michael Engelmann <zeprogramfactory@outlook.com>
2019-04-17 17:56:49 +0200

    Add Pause(), Play(), Stop()

--- MediaManager/Platforms/Uap/MediaManagerImplementation.cs
+++ MediaManager/Platforms/Uap/MediaManagerImplementation.cs
@@ -2,12 +2,10 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
-using MediaManager.Audio;
 using MediaManager.Media;
 using MediaManager.Platforms.Uap.Media;
 using MediaManager.Playback;
 using MediaManager.Queue;
-using MediaManager.Video;
 using MediaManager.Volume;
 using Windows.Media.Core;
 using Windows.Media.Playback;
@@ -113,10 +111,19 @@ namespace MediaManager
             IsInitialized = true;
         }
 
-
         public override Task Pause()
         {
-            throw new NotImplementedException();
+            if (_player.PlaybackSession.PlaybackState == MediaPlaybackState.Paused)
+            {
+                _player.Play();
+            }
+            else
+            {
+                //ToDo: ME: Why not a play from zéro?
+                _player.Pause();
+            };
+
+            return Task.CompletedTask;
         }
 
         public override Task Play(IMediaItem mediaItem)
@@ -150,7 +157,7 @@ namespace MediaManager
                 case MediaLocation.FileSystem:
                     var du = _player.SystemMediaTransportControls.DisplayUpdater;
                     var storageFile = await StorageFile.GetFileFromPathAsync(mediaItem.MediaUri);
-                    var playbackType = ( mediaItem.MediaType == MediaType.Audio ? Windows.Media.MediaPlaybackType.Music : Windows.Media.MediaPlaybackType.Video );
+                    var playbackType = (mediaItem.MediaType == MediaType.Audio ? Windows.Media.MediaPlaybackType.Music : Windows.Media.MediaPlaybackType.Video);
                     await du.CopyFromFileAsync(playbackType, storageFile);
                     du.Update();
                     return MediaSource.CreateFromStorageFile(storageFile);
@@ -183,7 +190,9 @@ namespace MediaManager
 
         public override Task Play()
         {
-            throw new NotImplementedException();
+            _player.PlaybackSession.PlaybackRate = 1;
+            _player.Play();
+            return Task.CompletedTask;
         }
 
         public override Task<bool> PlayNext()
@@ -213,7 +222,13 @@ namespace MediaManager
 
         public override Task Stop()
         {
-            throw new NotImplementedException();
+            _player.PlaybackSession.PlaybackRate = 0;
+            _player.PlaybackSession.Position = TimeSpan.Zero;
+
+            _State = Playback.MediaPlayerState.Stopped;
+            this.OnStateChanged(this, new StateChangedEventArgs(_State));
+
+            return Task.CompletedTask;
         }
     }
 }
