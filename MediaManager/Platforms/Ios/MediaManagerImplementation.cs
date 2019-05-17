a3cc06bd5c21b5edd184db0c63546877c4c20529
Martijn van Dijk <mhvdijk@gmail.com>
2019-05-17 16:50:49 +0200

    Add remote commands

--- MediaManager/Platforms/Ios/MediaManagerImplementation.cs
+++ MediaManager/Platforms/Ios/MediaManagerImplementation.cs
@@ -1,11 +1,24 @@
-﻿namespace MediaManager
+﻿using MediaPlayer;
+using UIKit;
+
+namespace MediaManager
 {
     [Foundation.Preserve(AllMembers = true)]
     public class MediaManagerImplementation : AppleMediaManagerBase<MediaManager.Platforms.Ios.Media.MediaPlayer>
     {
         public MediaManagerImplementation()
         {
+            UIApplication.SharedApplication.BeginReceivingRemoteControlEvents();
 
+            var commandCenter = MPRemoteCommandCenter.Shared;
+            commandCenter.TogglePlayPauseCommand.Enabled = true;
+            commandCenter.TogglePlayPauseCommand.AddTarget(TogglePlayPauseCommand);
+        }
+
+        private MPRemoteCommandHandlerStatus TogglePlayPauseCommand(MPRemoteCommandEvent arg)
+        {
+            this.PlayPause();
+            return MPRemoteCommandHandlerStatus.Success;
         }
     }
 }
