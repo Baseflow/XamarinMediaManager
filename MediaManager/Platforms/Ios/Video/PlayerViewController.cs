79fb6eaa4073f0cac9b715ed23bf62c7d74a7ceb
Michal Orzechowski <michal@orzechowski.info.pl>
2019-07-26 13:14:11 +0200

    Detach VideoView on disappearing (iOS)

--- MediaManager/Platforms/Ios/Video/PlayerViewController.cs
+++ MediaManager/Platforms/Ios/Video/PlayerViewController.cs
@@ -4,9 +4,14 @@ namespace MediaManager.Platforms.Ios.Video
 {
     public class PlayerViewController : AVPlayerViewController
     {
+        protected MediaManagerImplementation MediaManager => CrossMediaManager.Apple;
+
         public override void ViewWillDisappear(bool animated)
         {
             base.ViewWillDisappear(animated);
+
+            if (MediaManager.MediaPlayer.VideoView == View.Superview)
+                MediaManager.MediaPlayer.VideoView = null;
 
             Player = null;
         }
