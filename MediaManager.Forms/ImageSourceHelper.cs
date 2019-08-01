c26fef819262f037f72ee020053217754908e5d7
Martijn van Dijk <mhvdijk@gmail.com>
2019-08-01 16:21:53 +0200

    Fix some bugs

---
+++ MediaManager.Forms/ImageSourceHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Xamarin.Forms;
+
+namespace MediaManager.Forms
+{
+    public static class ImageSourceHelper
+    {
+        public static ImageSource ToImageSource(this object image)
+        {
+#if ANDROID
+            if (image is Android.Graphics.Bitmap bitmap)
+                return MediaManager.Forms.Platforms.Android.ImageSourceHelper.ToImageSource(bitmap);
+#elif IOS
+            if (image is CoreGraphics.CGImage cgImage)
+                return MediaManager.Forms.Platforms.Ios.ImageSourceHelper.ToImageSource(cgImage);
+            else if (image is UIKit.UIImage uIImage)
+                return MediaManager.Forms.Platforms.Ios.ImageSourceHelper.ToImageSource(uIImage);
+#elif WINDOWS
+            //TODO: This one should not be async. It might deadlock
+            if (image is Windows.UI.Xaml.Media.Imaging.BitmapImage bitmapImage)
+                return MediaManager.Forms.Platforms.Uap.ImageSourceHelper.ToImageSource(bitmapImage).GetAwaiter().GetResult();
+#endif
+            return null;
+        }
+    }
+}
---
+++ MediaManager.Forms/Platforms/Uap/ImageSourceHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml.Media.Imaging;
+using Xamarin.Forms;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace MediaManager.Forms.Platforms.Uap
+{
+    public static class ImageSourceHelper
+    {
+        public static async Task<ImageSource> ToImageSource(this BitmapImage bitmapImage)
+        {
+            IRandomAccessStreamReference random = RandomAccessStreamReference.CreateFromUri(bitmapImage.UriSour‌​ce);
+            Windows.Graphics.Imaging.BitmapDecoder decoder = await Windows.Graphics.Imaging.BitmapDecoder.CreateAsync(await random.OpenReadAsync());
+            Windows.Graphics.Imaging.PixelDataProvider pixelData = await decoder.GetPixelDataAsync();
+            byte[] bitmapData = pixelData.DetachPixelData();
+
+            return ImageSource.FromStream(() => new MemoryStream(bitmapData));
+        }
+    }
+}
