a37be1c477ec9c49a274ac4c31439837458e2f28
Martijn van Dijk <mhvdijk@gmail.com>
2019-08-02 13:05:11 +0200

    Reattach video on appear

--- MediaManager/Platforms/Ios/Video/PlayerViewController.cs
+++ MediaManager/Platforms/Ios/Video/PlayerViewController.cs
@@ -6,6 +6,14 @@ namespace MediaManager.Platforms.Ios.Video
     {
         protected static MediaManagerImplementation MediaManager => CrossMediaManager.Apple;
 
+        public override void ViewWillAppear(bool animated)
+        {
+            base.ViewWillAppear(animated);
+
+            if (MediaManager.MediaPlayer.AutoAttachVideoView)
+                MediaManager.MediaPlayer.VideoView = View.Superview as VideoView;
+        }
+
         public override void ViewWillDisappear(bool animated)
         {
             base.ViewWillDisappear(animated);
