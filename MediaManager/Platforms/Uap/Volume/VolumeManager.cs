927ebb1f2fe3732fc147aabed5834a4f343e3a43
Andy Joiner <andy.joiner@tekkies.co.uk>
2019-08-04 17:03:06 -0700

    UWP: Volume, MaxVolume, Mute and VolumeChanged seem to work

--- MediaManager/Platforms/Uap/Volume/VolumeManager.cs
+++ MediaManager/Platforms/Uap/Volume/VolumeManager.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿﻿using System;
+﻿using Windows.Media.Playback;
 using MediaManager.Volume;
 
 namespace MediaManager.Platforms.Uap.Volume
@@ -6,8 +7,10 @@ namespace MediaManager.Platforms.Uap.Volume
     public class VolumeManager : VolumeManagerBase, IVolumeManager
     {
         protected MediaManagerImplementation MediaManager = CrossMediaManager.Windows;
+        protected MediaPlayer MediaPlayer => MediaManager.WindowsMediaPlayer.Player;
 
-        public override event VolumeChangedEventHandler VolumeChanged;
+        protected const int VolumeRangeMax = 100;
+        protected const int VolumeRangeMin = 0;
 
         public VolumeManager()
         {
@@ -15,19 +18,66 @@ namespace MediaManager.Platforms.Uap.Volume
             MediaManager.Player.IsMutedChanged += Player_IsMutedChanged;
         }
 
+        public override int CurrentVolume
+        {
+            get => FromNative(MediaPlayer.Volume);
+            set
+            {
+                int volume = value;
+                volume = Clamp(volume, VolumeRangeMin, MaxVolume);
+                MediaPlayer.Volume = ToNative(volume);
+                VolumeChanged?.Invoke(this, new VolumeChangedEventArgs(volume, Muted));
+            }
+        }
+
+        private int _maxVolume = VolumeRangeMax;
+        public override int MaxVolume
+        {
+            get => _maxVolume;
+            set
+            {
+                _maxVolume = value;
+                if (CurrentVolume > _maxVolume)
+                    CurrentVolume = _maxVolume;
+            }
+        }
+
+        public override bool Muted
+        {
+            get => MediaPlayer?.IsMuted ?? false;
+            set
+            {
+                MediaPlayer.IsMuted = value;
+                var volume = FromNative(MediaPlayer.Volume);
+                VolumeChanged?.Invoke(this, new VolumeChangedEventArgs(volume, value));
+            }
+        }
+
+        public override event VolumeChangedEventHandler VolumeChanged;
+
         private void Player_IsMutedChanged(Windows.Media.Playback.MediaPlayer sender, object args)
         {
-            Muted = sender.IsMuted;
+            //Muted = sender.IsMuted;
         }
 
         private void Player_VolumeChanged(Windows.Media.Playback.MediaPlayer sender, object args)
         {
-            CurrentVolume = Convert.ToInt32(sender.Volume);
+            //CurrentVolume = Convert.ToInt32(sender.Volume);
         }
 
-        public override int CurrentVolume { get; set; }
-        public override int MaxVolume { get; set; }
-        public override float Balance { get; set; }
-        public override bool Muted { get; set; }
+        protected int FromNative(double nativeVolume)
+        {
+            return (int)(nativeVolume * VolumeRangeMax);
+        }
+
+        protected double ToNative(int volume)
+        {
+            return volume / (double)VolumeRangeMax;
+        }
+
+        protected int Clamp(int value, int min, int max)
+        {
+            return (value < min) ? min : (value > max) ? max : value;
+        }
     }
 }
