376d8137596a140f9c99cd9f2964e1858f0b7655
Martijn van Dijk <mhvdijk@gmail.com>
2019-08-07 16:35:31 +0200

    Add mediaItem dictionary helper

--- MediaManager/Media/MediaItemExtensions.cs
+++ MediaManager/Media/MediaItemExtensions.cs
@@ -1,7 +1,21 @@
-﻿namespace MediaManager.Media
+﻿using System.Collections.Generic;
+
+namespace MediaManager.Media
 {
     public static class MediaItemExtensions
     {
+        public static IDictionary<string, object> ToDictionary(this IMediaItem mediaItem)
+        {
+            var mediaItemDict = new Dictionary<string, object>();
+            foreach (var item in mediaItem.GetType().GetProperties())
+            {
+                var value = item.GetValue(mediaItem, null);
+                if (value != null)
+                    mediaItemDict.Add(item.Name, value);
+            }
+            return mediaItemDict;
+        }
+
         public static string GetTitle(this IMediaItem mediaItem)
         {
             if (!string.IsNullOrEmpty(mediaItem.DisplayTitle))
