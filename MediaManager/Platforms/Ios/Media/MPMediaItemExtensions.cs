b8593cd5e48ddffa9b112485db03430084b3ff6e
Martijn van Dijk <mhvdijk@gmail.com>
2020-09-04 14:06:28 +0200

    Update files

--- MediaManager/Platforms/Ios/Media/MPMediaItemExtensions.cs
+++ MediaManager/Platforms/Ios/Media/MPMediaItemExtensions.cs
@@ -11,9 +11,12 @@ namespace MediaManager.Platforms.Ios.Media
     {
         public static IMediaItem ToMediaItem(this MPMediaItem item)
         {
+            if (item == null)
+                return null;
+#if IOS
             var output = new MediaItem
             {
-                MediaType = MediaType.Audio,
+                MediaType = item.MediaType.ToMediaType(),
                 Album = item.AlbumTitle,
                 Artist = item.Artist,
                 Compilation = null,
@@ -37,15 +40,39 @@ namespace MediaManager.Platforms.Ios.Media
             
             if (output.Date != null)
                 output.Year = output.Date.Year;
-            
+#elif TVOS
+            var output = new MediaItem();
+            //TODO: something like this?
+            /*
+            output.MediaType = item.ValueForKey(MPMediaItem.MediaTypeProperty);
+            output.Album = item.ValueForProperty(MPMediaItem.AlbumTitleProperty).ToString();
+            output.Artist = item.Artist,
+            output.Compilation = null,
+            output.Composer = item.Composer,
+            output.Duration = TimeSpan.FromSeconds(item.PlaybackDuration),
+            output.Genre = item.Genre,
+            output.Title = item.Title,
+            output.AlbumArtist = item.AlbumArtist,
+            output.DiscNumber = item.DiscNumber,
+            output.MediaUri = item.AssetURL.ToString(),
+            output.NumTracks = item.AlbumTrackCount,
+            output.UserRating = item.Rating,
+            output.Id = item.PersistentID.ToString()*/
+
+#endif
             return output;
         }
         
         public static IEnumerable<IMediaItem> ToMediaItems(this IEnumerable<MPMediaItem> items)
         {
+#if IOS
             return items
                 .Where(i => i.AssetURL != null && i.IsCloudItem == false && i.HasProtectedAsset == false)
                 .Select(i => i.ToMediaItem());
+#elif TVOS
+            return items
+                .Select(i => i.ToMediaItem());
+#endif
         }
     }
 }
---
+++ MediaManager/Platforms/Ios/Media/MPMediaTypeExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediaManager.Library;
+using MediaPlayer;
+
+namespace MediaManager.Platforms.Ios.Media
+{
+    public static class MPMediaTypeExtensions
+    {
+        public static MediaType ToMediaType(this MPMediaType mediaType)
+        {
+            switch (mediaType)
+            {
+                case MPMediaType.Music:
+                case MPMediaType.Podcast:
+                case MPMediaType.AudioBook:
+                case MPMediaType.AudioITunesU:
+                case MPMediaType.AnyAudio:
+                    return MediaType.Audio;
+                case MPMediaType.Movie:
+                case MPMediaType.TVShow:
+                case MPMediaType.VideoPodcast:
+                case MPMediaType.MusicVideo:
+                case MPMediaType.VideoITunesU:
+                case MPMediaType.HomeVideo:
+                case MPMediaType.TypeAnyVideo:
+                    return MediaType.Video;
+                case MPMediaType.Any:
+                default:
+                    return MediaType.Default;
+            }
+        }
+    }
+}
