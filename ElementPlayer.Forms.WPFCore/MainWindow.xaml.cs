68388e7f4783c771b10c9353f3c6449e52059b97
tuke307 <tonymeissner70@gmail.com>
2020-09-07 16:14:48 +0200

    try add dotnetcore support

---
+++ ElementPlayer.Forms.WPFCore/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using MediaManager;
+using MvvmCross.Platforms.Wpf.Views;
+
+namespace ElementPlayer.Forms.WPFCore
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MvxWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            CrossMediaManager.Current.Init();
+        }
+    }
+}
