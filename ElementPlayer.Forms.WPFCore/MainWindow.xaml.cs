e81fedd3a11979b2af76602609ddca429069e834
tuke307 <tonymeissner70@gmail.com>
2020-10-21 11:24:44 +0200

    some fixes

--- ElementPlayer.Forms.WPFCore/MainWindow.xaml.cs
+++ ElementPlayer.Forms.WPFCore/MainWindow.xaml.cs
@@ -1,12 +1,12 @@
 ﻿using MediaManager;
-using MvvmCross.Platforms.Wpf.Views;
+using MvvmCross.Forms.Platforms.Wpf.Views;
 
 namespace ElementPlayer.Forms.WPFCore
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : MvxWindow
+    public partial class MainWindow : MvxFormsWindowsPage
     {
         public MainWindow()
         {
