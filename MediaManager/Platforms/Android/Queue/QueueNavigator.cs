3e59d89e1d2b7cabc5af6762ab930116c319aae1
Jan Wiebe de Haan <janwiebe@jump.nl>
2021-04-06 09:23:50 +0200

    hotfix the AndroidMediaPlayer to expose the QueueNavigator. Add fallback delegate to QueueNavigator that allows to provide the media item when it can't be found.

--- MediaManager/Platforms/Android/Queue/QueueNavigator.cs
+++ MediaManager/Platforms/Android/Queue/QueueNavigator.cs
@@ -5,6 +5,7 @@ using Android.Support.V4.Media;
 using Android.Support.V4.Media.Session;
 using Com.Google.Android.Exoplayer2;
 using Com.Google.Android.Exoplayer2.Ext.Mediasession;
+using MediaManager.Library;
 using MediaManager.Platforms.Android.Media;
 
 namespace MediaManager.Platforms.Android.Queue
@@ -13,6 +14,8 @@ namespace MediaManager.Platforms.Android.Queue
     {
         protected MediaSessionCompat MediaSession { get; }
         protected MediaManagerImplementation MediaManager => CrossMediaManager.Android;
+
+        public Func<IPlayer, int, IMediaItem> MediaDescriptionRetriever { get; set; }
 
         public QueueNavigator(MediaSessionCompat mediaSession) : base(mediaSession)
         {
@@ -30,7 +33,12 @@ namespace MediaManager.Platforms.Android.Queue
 
         public override MediaDescriptionCompat GetMediaDescription(IPlayer player, int windowIndex)
         {
-            return MediaManager.Queue.ElementAtOrDefault(windowIndex)?.ToMediaDescription();
+            var mediaItem = MediaManager.Queue.ElementAtOrDefault(windowIndex);
+            if(mediaItem == null && MediaDescriptionRetriever != null)
+            {
+                mediaItem = MediaDescriptionRetriever.Invoke(player, windowIndex);
+            }
+            return mediaItem?.ToMediaDescription();
         }
     }
 }
