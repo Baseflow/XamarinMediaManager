e9c7fe247e71ceca031360e00dee7af493929130
barisy <barisy@biscozum.com.tr>
2021-06-08 18:23:17 +0300

    #629 ios13+ full screen problem

--- MediaManager/Platforms/Ios/Video/PlayerViewController.cs
+++ MediaManager/Platforms/Ios/Video/PlayerViewController.cs
@@ -1,9 +1,18 @@
 ﻿using AVKit;
+using CoreGraphics;
+using Foundation;
 
 namespace MediaManager.Platforms.Ios.Video
 {
     public class PlayerViewController : AVPlayerViewController
     {
+        private bool keepPlayer;
+
+        public PlayerViewController()
+        {
+            this.AddObserver(this, "contentOverlayView.frame", NSKeyValueObservingOptions.OldNew, Handle);
+        }
+
         protected static MediaManagerImplementation MediaManager => CrossMediaManager.Apple;
 
         public override void ViewWillAppear(bool animated)
@@ -22,8 +31,51 @@ namespace MediaManager.Platforms.Ios.Video
             {
                 MediaManager.MediaPlayer.VideoView = null;
             }
+            if (!keepPlayer)
+                Player = null;
+        }
 
-            Player = null;
+        public override void ObserveValue(NSString keyPath, NSObject ofObject, NSDictionary change, System.IntPtr context)
+        {
+
+            if (keyPath == "contentOverlayView.frame" && change != null && change[ChangeOldKey] != null)
+            {
+                var oldNsValue = change[ChangeOldKey];
+                var newNsValue = change[ChangeNewKey];
+                if (oldNsValue != null && newNsValue != null)
+                {
+                    var oldValue = (oldNsValue as NSValue).CGRectValue;
+                    var newValue = (newNsValue as NSValue).CGRectValue;
+                    if (oldValue != CGRect.Empty && oldValue != CGRect.Null && !oldValue.Equals(newValue))
+                    {
+                        //if portrait
+                        if (UIKit.UIScreen.MainScreen.Bounds.Height > UIKit.UIScreen.MainScreen.Bounds.Width)
+                        {
+                            if (newValue.Height < oldValue.Height)
+                            {
+                                //normalScreen so you can empty the player 
+                                keepPlayer = false;
+                            }
+                            else
+                            {
+                                //fullScreen so you need to keep player.
+                                keepPlayer = true;
+                            }
+                        }
+                        else
+                        {
+                            if (newValue.Width < UIKit.UIScreen.MainScreen.Bounds.Width)
+                            {
+                                keepPlayer = false;
+                            }
+                            else
+                            {
+                                keepPlayer = true;
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
